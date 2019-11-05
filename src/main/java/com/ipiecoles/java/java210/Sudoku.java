e92b222b70acb47f999d1d47978806b08329be5e
Nasim <Nasim@10.251.61.42>
2019-11-05 09:12:20 +0100

    last exo

--- src/main/java/com/ipiecoles/java/java210/Sudoku.java
+++ src/main/java/com/ipiecoles/java/java210/Sudoku.java
@@ -1,96 +1,120 @@
 package com.ipiecoles.java.java210;
 
+import java.util.Scanner;
+
 public class Sudoku {
 
-	/**
-	 * Constructeur par défaut
-	 */
+	public static final String FIN_SAISIE = "FIN";
+	public static boolean resolu = false;
+	public static short sudokuAResoudre[][];
+	private static Scanner entered;
+
+	public short[][] getSudokuAResoudre() {
+		return sudokuAResoudre;
+	}
+
+	public void setSudokuAResoudre(short[][] entry) {
+		sudokuAResoudre = entry;
+	}
+
 	public Sudoku() {
+		sudokuAResoudre = new short[9][9];
 	}
 
 	public static boolean ligneSaisieEstCoherente(String ligneSaisie) {
+		if (ligneSaisie == null || ligneSaisie.trim().isEmpty()) {
+			System.out.println(
+					"Les coordonnées du chiffre et/ou sa valeur ne peuvent pas être nulles, vides ou remplies avec des espaces");
+			return false;
+		}
+		if (ligneSaisie.length() != 3) {
+			System.out.println("Les coordonnées du chiffre et/ou sa valeur doit faire 3 caractères");
+			return false;
+		}
+		if (!ligneSaisie.substring(0, 1).matches("[0-8]") || !ligneSaisie.substring(1, 2).matches("[0-8]")
+				|| !ligneSaisie.substring(2, 3).matches("[1-9]")) {
+			// if(!ligneSaisie.matches("[0-8][0-8][19]" on peut faire ensemble
+			System.out.println("L'abscisse et l'ordonnée doivent être compris entre 0 et 8, la valeur entre 1 et 9");
+			return false;
+		}
 		return true;
 	}
-	
-	/**
-	 * Cette méthode invite l'utilisateur à saisir un ensemble de coordonnées pour initialiser un sudoku à résoudre.
-	 * Les coordonnées prennent la forme XYZ avec X correspondant à l'abscisse, Y l'ordonnée et Z la valeur. Seules les
-	 * chiffres présents sont à saisir et l'utilisateur doit appuyer sur entrée après chaque saisie. 
-	 * Lorsqu'il a terminé sa saisie, il entre la chaîne FIN. La fonction remplit au fur et à mesure un tableau de String
-	 * comportant les coordonnées des chiffres saisis.
-	 * 
-	 * A noter que pour chaque ligne saisie, sa cohérence est vérifiée en appelant la méthode ligneSaisieEstCoherente
-	 * En cas de mauvaise saisie, la saisie ne doit pas être prise en compte et l'utilisateur doit pouvoir saisie une nouvelle ligne
-	 * La fonction doit également gérer le cas où l'utilisateur ne rentre rien mais appuye sur Entrée
-	 *
-	 * @return Un tableau comportant les coordonnées des chiffres présents dans le sudoku à résoudre
-	 */
+
 	public static String[] demandeCoordonneesSudoku() {
-		return null;
+		entered = new Scanner(System.in);
+		String[] tableauCoordonnees = new String[81];
+		int indiceTableauCoordonnees = 0;
+		String ligneSaisie = null;
+		do {
+			System.out.println("Entrez les numéros (XYZ): ");
+			ligneSaisie = entered.nextLine();
+			if (ligneSaisie.equals(FIN_SAISIE)) {
+				break;
+			} else if (ligneSaisieEstCoherente(ligneSaisie)) {
+				tableauCoordonnees[indiceTableauCoordonnees++] = ligneSaisie;
+			} else {
+				System.out.println(
+						"Les coordonnées du chiffre et/ou sa valeur ne sont pas cohérents. Merci de réessayer");
+			}
+		} while (!ligneSaisie.equals(FIN_SAISIE) && indiceTableauCoordonnees < 81);
+		System.out.println("Merci, au revoir");
+		entered.close();
+		return tableauCoordonnees;
 	}
-	
-	/**
-	 * La méthode prend un tableau de coordonnées de chiffre soud la forme XYZ avec X correspondant 
-	 * à l'abscisse, Y l'ordonnée et Z la valeur et remplit le tableau sudokuAResoudre avec les bonnes valeurs
-	 * au bon endroit. Ex 012, première ligne deuxième colonne, on met la valeur 2. Lorsqu'une valeur nulle est 
-	 * rencontrée dans le tableau, on arrête le traitement
-	 * 
-	 * Pour passer d'une String à un short, on pourra utiliser la méthode stringToInt(string)
-	 * 
-	 * @param tableauCoordonnees
-	 */
+
 	public void remplitSudokuATrous(String[] tableauCoordonnees) {
-		
-    }
-	
+		for (String coordonne : tableauCoordonnees) {
+			if (coordonne == null) {
+				break;
+			}
+			short ligne = (short) stringToInt(coordonne.substring(0, 1));
+			short colonne = (short) stringToInt(coordonne.substring(1, 2));
+			short valeur = (short) stringToInt(coordonne.substring(2, 3));
+			sudokuAResoudre[ligne][colonne] = valeur;
+		}
+	}
+
 	private int stringToInt(String s) {
 		return Integer.parseInt(s);
 	}
-	
-	/**
-	 * Cette méthode affiche un sudoku de manière formatée sur la console.
-	 * Cela doit ressembler exactement à :
-	 * -----------------------
-	 * |   8   | 4   2 |   6   |
-	 * |   3 4 |       | 9 1   |
-	 * | 9 6   |       |   8 4 |
-	 *  -----------------------
-	 * |       | 2 1 6 |       |
-	 * |       |       |       |
-	 * |       | 3 5 7 |       |
-	 *  -----------------------
-	 * | 8 4   |       |   7 5 |
-	 * |   2 6 |       | 1 3   |
-	 * |   9   | 7   1 |   4   |
-	 *  -----------------------
-	 * 
-	 * @param sudoku tableau de short représentant les valeurs d'un sudoku (résolu ou non). 
-	 * Ce tableau fait 9 par 9 et contient des chiffres de 0 à 9, 0 correspondant à une valeur 
-	 * non trouvée (dans ce cas, le programme affiche un blanc à la place de 0
-	 */
+
 	public void ecrireSudoku(short[][] sudoku) {
-		
-    }
-	
-	/**
-	 * Cette méthode vérifie si un chiffre est autorisé à la position d'abscisse et
-	 * d'ordonnée donnés dans le sudoku en appliquant les règles suivantes : 
-	 * 
-	 * 1 : Si la valeur est déjà dans la ligne, le chiffre n'est pas autorisé
-	 * 2 : Si le valeur est déjà dans la colone, le chiffre n'est pas autorisé
-	 * 3 : Si la valeur est est déjà dans la boite, le chiffre n'est pas autorisé
-	 * 
-	 * @param abscisse
-	 * @param ordonnee
-	 * @param chiffre
-	 * @param sudoku
-	 * @return
-	 */
+		for (int i = 0; i < 9; i++) {
+			for (int j = 0; j < 9; j++) {
+				System.out.print(" " + sudoku[i][j] + " ");
+			}
+			System.out.println(" ");
+		}
+
+	}
+
 	public static boolean estAutorise(int abscisse, int ordonnee, short chiffre, short[][] sudoku) {
+		for (int j = 0; j < 9; j++) {
+			if (chiffre == sudoku[abscisse][j]) {
+				return false;
+			}
+		}
+
+		for (int i = 0; i < 9; i++) {
+			if (chiffre == sudoku[i][ordonnee]) {
+				return false;
+			}
+		}
+
+		int decI = (abscisse / 3) * 3;
+		int decJ = (ordonnee / 3) * 3;
+		for (int i = 0; i < 3; i++) {
+			for (int j = 0; j < 3; j++) {
+				if (chiffre == sudoku[i][j]) {
+					return false;
+				}
+			}
+		}
+
 		return true;
-    }
+	}
 
 	public boolean resoudre(int abscisse, int ordonnee, short[][] sudoku) {
-		return true;
-    }
+		
+	}
 }
